ed350ccfbb89887ea50d118bf7c8f08bd22481a8
Ra Naun <dmitriy.slavskiy@hotger.com>
2022-04-02 13:29:53 +0300

    init_pages

---
+++ app/src/pages/storage/index.jsx
@@ -0,0 +1,9 @@
+import { Container, Row, Card, Button } from 'react-bootstrap';
+
+export default function Home() {
+  return (
+    <Container className="md-container">
+      storage
+    </Container>
+  );
+}
