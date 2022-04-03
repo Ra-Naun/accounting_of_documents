c96771c45f412b1ccd632465c5125a82d1ebf1a2
Ra Naun <dmitriy.slavskiy@hotger.com>
2022-04-03 14:45:19 +0300

    fix

--- app/src/db/seeders/20220402020031-user.js
+++ app/src/db/seeders/20220402020031-user.js
@@ -1,6 +1,10 @@
-const password = '$2a$10$mj7wHBUOGMEehzzjb6xmKegcIjLfYDXBSi3LzEIzA4qqAOibSdBCq';
+const password = '$2a$10$uQgdIbW7.bzqp/ZaS6tWkOYVjyHo9ik0Ux5CF6P5rP4.u4cwe/Aca'; // 12345
 const storageArr = [
-  { name: 'Admin', secondName: 'Adminoff',	email: 'dmitriy_slavskiy@mail.ru', password,	phoneNumber: 89381044707,	role_id: 1,	isActive: true },
+  { name: 'Admin', secondName: 'Adminoff', email: 'dmitriy_slavskiy@mail.ru', password, phoneNumber: 89381044707, role_id: 1, isActive: true },
+  { name: 'Alex', secondName: 'Al', email: 'alex@mail.ru', password, phoneNumber: 89381234707, role_id: 2, isActive: false },
+  { name: 'Alex', secondName: 'Al', email: 'alex@mail.ru', password, phoneNumber: 89381234707, role_id: 2, isActive: false },
+  { name: 'Alex', secondName: 'Al', email: 'alex@mail.ru', password, phoneNumber: 89381234707, role_id: 2, isActive: false },
+  { name: 'Alex', secondName: 'Al', email: 'alex@mail.ru', password, phoneNumber: 89381234707, role_id: 2, isActive: false },
 ];
 
 module.exports = {
