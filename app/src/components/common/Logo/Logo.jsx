a126b9269b69d41b8d68dafd6b864792f7184f37
Ra Naun <dmitriy.slavskiy@hotger.com>
2022-04-03 15:53:37 +0300

    fix

--- app/src/components/common/Logo/Logo.jsx
+++ app/src/components/common/Logo/Logo.jsx
@@ -1,12 +1,12 @@
 import { Container, Button } from 'react-bootstrap';
 import Image from 'next/image';
+import { signOut } from 'next-auth/client';
 import LogoImg from './images/logo.svg';
-import { signOut } from 'next-auth/client'
 
 const Logo = () => (
     <Container className='d-flex align-items-center justify-content-between'>
       <Image src={LogoImg} alt="Home page" width={150} height={100} className="logo" />
-      <Button onClick={() => signOut({callbackUrl: 'https://server.kinsle.ru/login'})}>Выйти из аккаунта</Button>
+      <Button onClick={() => signOut({})}>Выйти из аккаунта</Button>
     </Container>
   );
 
